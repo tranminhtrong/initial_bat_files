b07d73b5666cbf7c016c014330c49afab858e361
Tran <ttran229@vdc.csc.com>
2017-09-26 09:37:12 +0700

    add Ionic_initial_project.bat/ Git_initial_repo.bat

---
+++ Git_initial_repo.bat
@@ -0,0 +1,5 @@
+git init
+git remote add https://github.com/tranminhtrong/initial_bat_files.git
+git add -A
+git commit -m "add Ionic_initial_project.bat/ Git_initial_repo.bat"
+git push --set-upstream origin master
