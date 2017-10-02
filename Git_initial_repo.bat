63a6f037e731a3f423dda087473389020957dbc1
Tran <ttran229@vdc.csc.com>
2017-10-02 11:23:08 +0700

    update git.bat

--- Git_initial_repo.bat
+++ Git_initial_repo.bat
@@ -1,5 +1,5 @@
 git init
-git remote add https://github.com/tranminhtrong/initial_bat_files.git
+git remote add origin https://github.com/tranminhtrong/initial_bat_files.git
 git add -A
 git commit -m "add Ionic_initial_project.bat/ Git_initial_repo.bat"
-git push --set-upstream origin master
+git push -u --set-upstream origin master
